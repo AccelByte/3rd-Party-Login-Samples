d7bb48873d63db60688c4f11ca4e7882bc8626cb
TeoFerdian <teoderikus@accelbyte.net>
2020-05-28 14:29:14 +0700

    feat(main): adding example for Xbox One login implementation

--- Assets/thirdPartyLogin.cs
+++ Assets/thirdPartyLogin.cs
@@ -1,26 +1,34 @@
 ﻿using AccelByte.Api;
+#if ENABLE_WINMD_SUPPORT
+using Microsoft.Xbox.Services;
+#endif
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System;
+​
 public class thirdPartyLogin : MonoBehaviour
 {
+#if ENABLE_WINMD_SUPPORT
+    Windows.Xbox.System.User mCurrentUser = null;
+    XboxLiveContext mContext = null;
+#endif
     public InputField Logs;
     // Start is called before the first frame update
     void Start()
     {
 #if UNITY_PS4
         Sony.NP.InitToolkit init = new Sony.NP.InitToolkit();
-
+​
         try
         {
             init.SetPushNotificationsFlags(Sony.NP.PushNotificationsFlags.NewGameDataMessage | Sony.NP.PushNotificationsFlags.NewInGameMessage |
                                     Sony.NP.PushNotificationsFlags.NewInvitation | Sony.NP.PushNotificationsFlags.UpdateBlockedUsersList |
                                     Sony.NP.PushNotificationsFlags.UpdateFriendPresence | Sony.NP.PushNotificationsFlags.UpdateFriendsList);
-
+​
             Sony.NP.Main.Initialize(init);
-
+​
             Debug.LogAssertion("No exception was thrown even though NpToolkit should have already been initialized.");
             Logs.text += "\n NpToolkit has been initialized.";
         }
@@ -36,32 +44,83 @@ public class thirdPartyLogin : MonoBehaviour
             Logs.text += "\n NpToolkit failed to initialized. Error: " + e.Message;
         }
 #endif
+#if ENABLE_WINMD_SUPPORT
+        mCurrentUser = Windows.Xbox.ApplicationModel.Core.CoreApplicationContext.CurrentUser;
+        mContext = new XboxLiveContext(mCurrentUser);
+#endif
+#if UNITY_PS4 || ENABLE_WINMD_SUPPORT
         Logs.text += "\n Start Sign-in";
         SignIn();
+#endif
     }
-
+​
     // Update is called once per frame
     void Update()
     {
     }
+​
+#if ENABLE_WINMD_SUPPORT
+​
+    void SignIn()
+    {
+        try
+        {
+            //TODO: URL needs to be changed
+            string url = "";​
+            var result = mCurrentUser.GetTokenAndSignatureAsync("POST", url, "");
+            Logs.text += "\n After GetTokenAndSignatureAsync...." + result;
+​
+            while (result.Status != Windows.Foundation.AsyncStatus.Completed)
+            {
+                Logs.text += "\n Waiting...." + result.Status;
+                System.Threading.Thread.Sleep(1000);
+            }
+​
+            Logs.text += "\nReturn Code: " + result.ErrorCode;
+            Logs.text += "\nAuthCode: " + result.GetResults().Token;
+            var user = AccelBytePlugin.GetUser();
+            Logs.text += "\nLogin to AB";
+            user.LoginWithOtherPlatform(AccelByte.Models.PlatformType.Live, result.GetResults().Token, loginResult =>
+            {
+                if (!loginResult.IsError)
+                {
+                    Logs.text += "\nLogin Success!";
+                    user.GetData(resultData =>
+                    {
+                        if (!resultData.IsError)
+                        {
+                            Logs.text += "\nUserId: " + resultData.Value.userId;
+                            Logs.text += "\nDisplayName: " + resultData.Value.displayName;
+                        }
+                    });
+                }
+            });
 
+        }
+        catch (Exception ex)
+        {
+            Logs.text += "\n Exception...." + ex;
+            return;
+        }
+    }
+#endif
 #if UNITY_PS4
     void SignIn()
     {
         try
         {
             Sony.NP.Auth.GetAuthCodeRequest request = new Sony.NP.Auth.GetAuthCodeRequest();
-
+​
             // test values from SDK nptoolkit sample ... replace with your own project values
             Sony.NP.Auth.NpClientId clientId = new Sony.NP.Auth.NpClientId();
             clientId.Id = "";
-
+​
             request.ClientId = clientId;
             request.Scope = "psn:s2s";
             request.UserId = GetLocalProfiles();
-
+​
             Sony.NP.Auth.AuthCodeResponse response = new Sony.NP.Auth.AuthCodeResponse();
-
+​
             int requestId = Sony.NP.Auth.GetAuthCode(request, response);
             while (response.Locked)
             {
@@ -95,15 +154,15 @@ public class thirdPartyLogin : MonoBehaviour
             Logs.text+= ("\nException : " + e.ExtendedMessage);
         }
     }
-
+​
     public Sony.NP.Core.UserServiceUserId GetLocalProfiles()
     {
         Sony.NP.UserProfiles.LocalUsers users = new Sony.NP.UserProfiles.LocalUsers();
-
+​
         try
         {
             Logs.text+=("\nGet Local Profiles");
-
+​
             Sony.NP.UserProfiles.GetLocalUsers(users);
         }
         catch (Sony.NP.NpToolkitException e)
@@ -114,7 +173,7 @@ public class thirdPartyLogin : MonoBehaviour
                 return Sony.NP.Core.UserServiceUserId.UserIdInvalid;
             }
         }
-
+​
         // Even if an exception has occured check the results for each user. 
         for (int i = 0; i < users.LocalUsersIds.Length; i++)
         {
@@ -127,14 +186,14 @@ public class thirdPartyLogin : MonoBehaviour
                 else
                 {
                     string output = Sony.NP.Core.ConvertSceErrorToString(users.LocalUsersIds[i].SceErrorCode);
-
+​
                     // Some error code has been returned for the user. This may just mean they are not singed in or have not signed up for an online account.
                     Logs.text += ("\nUser id = " + users.LocalUsersIds[i].UserId.ToString() + " : Account id = " + users.LocalUsersIds[i].AccountId.ToString() + " : Error = " + output);
                 }
                     return users.LocalUsersIds[i].UserId;
             }
         }
-
+​
         return Sony.NP.Core.UserServiceUserId.UserIdInvalid;
     }
 #endif
--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 Prerequisites
+- PS4
 1. AccelByteUnitySDK ver 1.12.0 or newer
 2. Sony NPToolkit2 and Unity with PS4 SDK support. You can find it [here](https://ps4.siedev.net/forums/thread/227324/)
 
+- Xbox One
+1. AccelByteUnitySDK ver 1.12.1 or newer
+2. Unity with Xbox One Editor extension and Visual Studio Tools for Unity. You can follow [this documentation](https://docs.microsoft.com/en-us/gaming/xbox-live/get-started/setup-ide/managed-partners/unity-xbox/live-partner-unity-xdk-il2cpp)  
+
 Settings
+- PS4
 1. Change your build platform to PS4
 2. Set your App Content ID on Player Settings>Publishing Settings>Content ID. [Here](https://ps4.siedev.net/resources/documents/Misc/current/Publishing_Tools-Overview/0003.html) is some explanation about Content ID and Packages
 3. Set your NP Title ID on Player Settings>Publishing Settings>Playstation(R)Network>NP Title ID
-4. Fill you PS4 Client ID credential on file thirdPartyLogin.cs line 60
+4. Fill your PS4 Client ID credential on file thirdPartyLogin.cs. You can find your Client ID on your PS4 DevNet Dashboard.
 5. Don't forget to fill your AB Credentials too on AccelByteSDKConfig.json
+
+- Xbox One
+1. Change your build platform to Xbox One
+2. Set your Title ID and Service Config ID(SCID) on Player Settings>Publishing Settings. You can find your Title ID and SCID on your Microsoft Partner Center Dashboard.
+3. Fill your Xbox Live request url on file thirdPartyLogin.cs
+4. Don't forget to fill your AB Credentials too on AccelByteSDKConfig.json
