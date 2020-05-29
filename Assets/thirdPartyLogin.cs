4f37544e660f26c48cac0fda54dbb09d7192de8d
TeoFerdian <teoderikus@accelbyte.net>
2020-05-29 09:10:26 +0700

    fix(main): fixing unexpected symbol error

--- Assets/thirdPartyLogin.cs
+++ Assets/thirdPartyLogin.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
-​
+
 public class thirdPartyLogin : MonoBehaviour
 {
 #if ENABLE_WINMD_SUPPORT
@@ -20,15 +20,15 @@ public class thirdPartyLogin : MonoBehaviour
     {
 #if UNITY_PS4
         Sony.NP.InitToolkit init = new Sony.NP.InitToolkit();
-​
+
         try
         {
             init.SetPushNotificationsFlags(Sony.NP.PushNotificationsFlags.NewGameDataMessage | Sony.NP.PushNotificationsFlags.NewInGameMessage |
                                     Sony.NP.PushNotificationsFlags.NewInvitation | Sony.NP.PushNotificationsFlags.UpdateBlockedUsersList |
                                     Sony.NP.PushNotificationsFlags.UpdateFriendPresence | Sony.NP.PushNotificationsFlags.UpdateFriendsList);
-​
+
             Sony.NP.Main.Initialize(init);
-​
+
             Debug.LogAssertion("No exception was thrown even though NpToolkit should have already been initialized.");
             Logs.text += "\n NpToolkit has been initialized.";
         }
@@ -53,12 +53,12 @@ public class thirdPartyLogin : MonoBehaviour
         SignIn();
 #endif
     }
-​
+
     // Update is called once per frame
     void Update()
     {
     }
-​
+
 #if ENABLE_WINMD_SUPPORT
 ​
     void SignIn()
@@ -110,17 +110,17 @@ public class thirdPartyLogin : MonoBehaviour
         try
         {
             Sony.NP.Auth.GetAuthCodeRequest request = new Sony.NP.Auth.GetAuthCodeRequest();
-​
+
             // test values from SDK nptoolkit sample ... replace with your own project values
             Sony.NP.Auth.NpClientId clientId = new Sony.NP.Auth.NpClientId();
             clientId.Id = "";
-​
+
             request.ClientId = clientId;
             request.Scope = "psn:s2s";
             request.UserId = GetLocalProfiles();
-​
+
             Sony.NP.Auth.AuthCodeResponse response = new Sony.NP.Auth.AuthCodeResponse();
-​
+
             int requestId = Sony.NP.Auth.GetAuthCode(request, response);
             while (response.Locked)
             {
@@ -154,15 +154,15 @@ public class thirdPartyLogin : MonoBehaviour
             Logs.text+= ("\nException : " + e.ExtendedMessage);
         }
     }
-​
+
     public Sony.NP.Core.UserServiceUserId GetLocalProfiles()
     {
         Sony.NP.UserProfiles.LocalUsers users = new Sony.NP.UserProfiles.LocalUsers();
-​
+
         try
         {
             Logs.text+=("\nGet Local Profiles");
-​
+
             Sony.NP.UserProfiles.GetLocalUsers(users);
         }
         catch (Sony.NP.NpToolkitException e)
@@ -173,7 +173,7 @@ public class thirdPartyLogin : MonoBehaviour
                 return Sony.NP.Core.UserServiceUserId.UserIdInvalid;
             }
         }
-​
+
         // Even if an exception has occured check the results for each user. 
         for (int i = 0; i < users.LocalUsersIds.Length; i++)
         {
@@ -186,14 +186,13 @@ public class thirdPartyLogin : MonoBehaviour
                 else
                 {
                     string output = Sony.NP.Core.ConvertSceErrorToString(users.LocalUsersIds[i].SceErrorCode);
-​
+
                     // Some error code has been returned for the user. This may just mean they are not singed in or have not signed up for an online account.
                     Logs.text += ("\nUser id = " + users.LocalUsersIds[i].UserId.ToString() + " : Account id = " + users.LocalUsersIds[i].AccountId.ToString() + " : Error = " + output);
                 }
                     return users.LocalUsersIds[i].UserId;
             }
         }
-​
         return Sony.NP.Core.UserServiceUserId.UserIdInvalid;
     }
 #endif
