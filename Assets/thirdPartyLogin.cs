58a3c1ee5c84f3fa7feecd31a41fb209410cabf4
TeoFerdian <teoderikus@accelbyte.net>
2020-05-30 17:22:02 +0700

    fix(main): fix another symbol error and add PrintLog

--- Assets/thirdPartyLogin.cs
+++ Assets/thirdPartyLogin.cs
@@ -19,29 +19,28 @@ public class thirdPartyLogin : MonoBehaviour
     void Start()
     {
 #if UNITY_PS4
-        Sony.NP.InitToolkit init = new Sony.NP.InitToolkit();
-
         try
         {
-            init.SetPushNotificationsFlags(Sony.NP.PushNotificationsFlags.NewGameDataMessage | Sony.NP.PushNotificationsFlags.NewInGameMessage |
+            Sony.NP.Main.OnAsyncEvent += OnAsyncEvent;
+            Sony.NP.InitToolkit init = new Sony.NP.InitToolkit();
+            init.SetPushNotificationsFlags(Sony.NP.PushNotificationsFlags.NewInGameMessage |
                                     Sony.NP.PushNotificationsFlags.NewInvitation | Sony.NP.PushNotificationsFlags.UpdateBlockedUsersList |
                                     Sony.NP.PushNotificationsFlags.UpdateFriendPresence | Sony.NP.PushNotificationsFlags.UpdateFriendsList);
 
             Sony.NP.Main.Initialize(init);
 
-            Debug.LogAssertion("No exception was thrown even though NpToolkit should have already been initialized.");
-            Logs.text += "\n NpToolkit has been initialized.";
+            PrintLog("\n NpToolkit has been initialized.");
         }
         catch (Sony.NP.NpToolkitException e)
         {
             Debug.Log("Exception thrown - test past.");
-            Logs.text += "\n NpToolkit failed to initialized. Error: "+e.Message;
+            PrintLog("\n NpToolkit failed to initialized. Error: " +e.Message);
         }
         catch (System.Exception e)
         {
             // Unexcepted expection occured.
             Debug.LogException(e);
-            Logs.text += "\n NpToolkit failed to initialized. Error: " + e.Message;
+            PrintLog("\n NpToolkit failed to initialized. Error: " + e.Message);
         }
 #endif
 #if ENABLE_WINMD_SUPPORT
@@ -49,7 +48,7 @@ public class thirdPartyLogin : MonoBehaviour
         mContext = new XboxLiveContext(mCurrentUser);
 #endif
 #if UNITY_PS4 || ENABLE_WINMD_SUPPORT
-        Logs.text += "\n Start Sign-in";
+        PrintLog("\n Start Sign-in");
         SignIn();
 #endif
     }
@@ -60,37 +59,36 @@ public class thirdPartyLogin : MonoBehaviour
     }
 
 #if ENABLE_WINMD_SUPPORT
-​
     void SignIn()
     {
         try
         {
             //TODO: URL needs to be changed
-            string url = "";​
+            string url = "";
             var result = mCurrentUser.GetTokenAndSignatureAsync("POST", url, "");
-            Logs.text += "\n After GetTokenAndSignatureAsync...." + result;
-​
+            PrintLog("\n After GetTokenAndSignatureAsync...." + result);
+
             while (result.Status != Windows.Foundation.AsyncStatus.Completed)
             {
-                Logs.text += "\n Waiting...." + result.Status;
+                PrintLog("\n Waiting...." + result.Status);
                 System.Threading.Thread.Sleep(1000);
             }
-​
-            Logs.text += "\nReturn Code: " + result.ErrorCode;
-            Logs.text += "\nAuthCode: " + result.GetResults().Token;
+
+            PrintLog("\nReturn Code: " + result.ErrorCode);
+            PrintLog("\nAuthCode: " + result.GetResults().Token);
             var user = AccelBytePlugin.GetUser();
-            Logs.text += "\nLogin to AB";
+            PrintLog("\nLogin to AB");
             user.LoginWithOtherPlatform(AccelByte.Models.PlatformType.Live, result.GetResults().Token, loginResult =>
             {
                 if (!loginResult.IsError)
                 {
-                    Logs.text += "\nLogin Success!";
+                    PrintLog("\nLogin Success!");
                     user.GetData(resultData =>
                     {
                         if (!resultData.IsError)
                         {
-                            Logs.text += "\nUserId: " + resultData.Value.userId;
-                            Logs.text += "\nDisplayName: " + resultData.Value.displayName;
+                            PrintLog("\nUserId: " + resultData.Value.userId);
+                            PrintLog("\nDisplayName: " + resultData.Value.displayName);
                         }
                     });
                 }
@@ -99,7 +97,7 @@ public class thirdPartyLogin : MonoBehaviour
         }
         catch (Exception ex)
         {
-            Logs.text += "\n Exception...." + ex;
+            PrintLog("\n Exception...." + ex);
             return;
         }
     }
@@ -118,31 +116,32 @@ public class thirdPartyLogin : MonoBehaviour
             request.ClientId = clientId;
             request.Scope = "psn:s2s";
             request.UserId = GetLocalProfiles();
+            PrintLog("\nGet UserId Success! UserId: " + request.UserId);
 
             Sony.NP.Auth.AuthCodeResponse response = new Sony.NP.Auth.AuthCodeResponse();
-
             int requestId = Sony.NP.Auth.GetAuthCode(request, response);
             while (response.Locked)
             {
                 new WaitForSeconds(1);
             }
-            Logs.text += "\n Return Code: "+response.ReturnCode;
-            Logs.text += "\nIssuerId: " + response.IssuerId;
+            PrintLog("\n Return Code: " + response.ReturnCode);
+            PrintLog("\nIssuerId: " + response.IssuerId);
             if (!response.IsErrorCode)
             {
-                Logs.text += "\nAuthCode: " + response.AuthCode;
+                PrintLog("\nAuthCode: " + response.AuthCode);
                 var user = AccelBytePlugin.GetUser();
-                Logs.text += "\nLogin to AB";
+                PrintLog("\nLogin to AB");
                 user.LoginWithOtherPlatform(AccelByte.Models.PlatformType.PS4, response.AuthCode, result =>
                 {
-                    if (!result.IsError) {
-                        Logs.text += "\nLogin Success!";
+                    if (!result.IsError)
+                    {
+                        PrintLog("\nLogin Success!");
                         user.GetData(resultData =>
                         {
                             if (!resultData.IsError)
                             {
-                                Logs.text += "\nUserId: " + resultData.Value.userId;
-                                Logs.text += "\nDisplayName: " + resultData.Value.displayName;
+                                PrintLog("\nUserId: " + resultData.Value.userId);
+                                PrintLog("\nDisplayName: " + resultData.Value.displayName);
                             }
                         });
                     }
@@ -151,7 +150,7 @@ public class thirdPartyLogin : MonoBehaviour
         }
         catch (Sony.NP.NpToolkitException e)
         {
-            Logs.text+= ("\nException : " + e.ExtendedMessage);
+            PrintLog("\nException : " + e.ExtendedMessage);
         }
     }
 
@@ -161,7 +160,7 @@ public class thirdPartyLogin : MonoBehaviour
 
         try
         {
-            Logs.text+=("\nGet Local Profiles");
+            PrintLog("\nGet Local Profiles");
 
             Sony.NP.UserProfiles.GetLocalUsers(users);
         }
@@ -169,7 +168,7 @@ public class thirdPartyLogin : MonoBehaviour
         {
             if (e.SceErrorCode != (int)Sony.NP.Core.ReturnCodes.SUCCESS)
             {
-                Logs.text+=("\nException : " + e.ExtendedMessage);
+                PrintLog("\nException : " + e.ExtendedMessage);
                 return Sony.NP.Core.UserServiceUserId.UserIdInvalid;
             }
         }
@@ -181,19 +180,69 @@ public class thirdPartyLogin : MonoBehaviour
             {
                 if (users.LocalUsersIds[i].SceErrorCode == (int)Sony.NP.Core.ReturnCodes.SUCCESS)
                 {
-                    Logs.text+=("\nUser id = " + users.LocalUsersIds[i].UserId.ToString() + " : Account id = " + users.LocalUsersIds[i].AccountId.ToString());
+                    PrintLog("\nUser id = " + users.LocalUsersIds[i].UserId.ToString() + " : Account id = " + users.LocalUsersIds[i].AccountId.ToString());
                 }
                 else
                 {
                     string output = Sony.NP.Core.ConvertSceErrorToString(users.LocalUsersIds[i].SceErrorCode);
 
                     // Some error code has been returned for the user. This may just mean they are not singed in or have not signed up for an online account.
-                    Logs.text += ("\nUser id = " + users.LocalUsersIds[i].UserId.ToString() + " : Account id = " + users.LocalUsersIds[i].AccountId.ToString() + " : Error = " + output);
+                    PrintLog("\nUser id = " + users.LocalUsersIds[i].UserId.ToString() + " : Account id = " + users.LocalUsersIds[i].AccountId.ToString() + " : Error = " + output);
                 }
                     return users.LocalUsersIds[i].UserId;
             }
         }
         return Sony.NP.Core.UserServiceUserId.UserIdInvalid;
     }
+
+    public void OnAsyncEvent(Sony.NP.NpCallbackEvent callbackEvent)
+    {
+        if (callbackEvent.Service == Sony.NP.ServiceTypes.Auth)
+        {
+            switch (callbackEvent.ApiCalled)
+            {
+                case Sony.NP.FunctionTypes.AuthGetAuthCode:
+                    OutputAuthCode(callbackEvent.Response as Sony.NP.Auth.AuthCodeResponse);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void OutputAuthCode(Sony.NP.Auth.AuthCodeResponse response)
+    {
+        if (response == null) return;
+
+        PrintLog("Auth Code Response");
+
+        if (response.Locked == false)
+        {
+            PrintLog("AuthCode : " + response.AuthCode);
+            PrintLog("IssuerId : " + response.IssuerId);
+            var user = AccelBytePlugin.GetUser();
+            PrintLog("\nLogin to AB");
+            user.LoginWithOtherPlatform(AccelByte.Models.PlatformType.PS4, response.AuthCode, result =>
+            {
+                if (!result.IsError)
+                {
+                    PrintLog("\nLogin Success!");
+                    user.GetData(resultData =>
+                    {
+                        if (!resultData.IsError)
+                        {
+                            PrintLog("\nUserId: " + resultData.Value.userId);
+                            PrintLog("\nDisplayName: " + resultData.Value.displayName);
+                        }
+                    });
+                }
+            });
+        }
+    }
 #endif
+    public void PrintLog(string Message)
+    {
+        Logs.text += (Message);
+        Debug.Log(Message);
+    }
 }
